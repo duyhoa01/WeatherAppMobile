4359a06aa10587b3d5cb4044baf1fe6bff1b2269
Duc Hung <hungklyhongkl@gmail.com>
2022-06-01 16:14:41 +0700

    UISelectCity

--- WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
+++ WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+import com.example.weatherapp.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private ArrayList<String> citylist;
+    private CityAdapter cityAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        citylist = new ArrayList<String>();
+        citylist.add("Da Nang");
+        citylist.add("Ho Chi Minh");
+        citylist.add("Ha Tinh");
+        citylist.add("Hue");
+
+        cityAdapter = new CityAdapter(citylist);
+        binding.rvCitys.setAdapter(cityAdapter);
+        binding.rvCitys.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+        return true;
     }
 }
